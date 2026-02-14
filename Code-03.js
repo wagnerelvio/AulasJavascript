3a37267b969e734cad8233161f0e9433fe8a64e9
Wagner Elvio <wagnerelvio@gmail.com>
2026-02-14 19:03:58 -0300

    Operadores de atribuição

---
+++ Code-03.js
@@ -0,0 +1,15 @@
+// Atribuição composta
+let saldo = 100;
+console.log("saldo =", saldo);
+
+saldo += 50;
+console.log("saldo += 50 ->", saldo);
+
+saldo -= 25;
+console.log("saldo -= 25 ->", saldo);
+
+saldo *= 2;
+console.log("saldo *= 2 ->", saldo);
+
+saldo /= 5;
+console.log("saldo /= 5 ->", saldo);
