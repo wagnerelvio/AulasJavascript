5d546627a33d78454431104e0b23f3876bb1d0c5
Wagner Elvio <wagnerelvio@gmail.com>
2026-02-14 19:20:17 -0300

    Comandos if- else if-else

---
+++ Code-08.js
@@ -0,0 +1,10 @@
+// Comandos if- else if-else -- Classificar temperatura
+const temperatura = 18; // troque aqui
+
+if (temperatura <= 10) {
+  console.log("Frio");
+} else if (temperatura <= 20) {
+  console.log("Ameno");
+} else {
+  console.log("Quente");
+}
