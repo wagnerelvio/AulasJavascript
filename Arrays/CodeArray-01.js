d9acb0b0413f32ab404263cd34d3831beeea9ed4
wagnerelvio <wagnerelvio@gmail.com>
2026-02-15 20:51:35 -0300

    Create CodeArray-01.js

---
+++ Arrays/CodeArray-01.js
@@ -0,0 +1,6 @@
+console.log("\n=== Exercicio 1 ===");
+const ex1 = [12, 7, 25, 3, 18, 9, 40, 6];
+console.log("Array:", ex1);
+console.log("Primeiro:", ex1[0]);
+console.log("Ultimo:", ex1[ex1.length - 1]);
+console.log("Tamanho:", ex1.length);
