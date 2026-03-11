a7bc8e0131ff2a8951cfbe72b2fd8d622ff937f7
wagnerelvio <wagnerelvio@gmail.com>
2026-03-11 20:09:57 -0300

    Create Code-06.js

---
+++ 11032026/Code-06.js
@@ -0,0 +1,11 @@
+let nota = 7;
+
+if (nota >= 9) {
+  console.log("Excelente");
+} else if (nota >= 7) {
+  console.log("Aprovado");
+} else if (nota >= 5) {
+  console.log("Recuperação");
+} else {
+  console.log("Reprovado");
+}
