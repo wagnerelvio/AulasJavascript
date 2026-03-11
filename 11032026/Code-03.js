03769223db1c6589a47d6d9a1473dc344a83a7de
wagnerelvio <wagnerelvio@gmail.com>
2026-03-11 13:05:41 -0300

    Create Code-03.js

---
+++ 11032026/Code-03.js
@@ -0,0 +1,9 @@
+let n2 = "10";
+
+console.log(n1 == n2);   // true
+console.log(n1 === n2);  // false
+console.log(n1 != 5);    // true
+console.log(n1 > 7);     // true
+console.log(n1 < 20);    // true
+console.log(n1 >= 10);   // true
+console.log(n1 <= 9);    // false
