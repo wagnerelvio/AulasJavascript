1f1065a6d6a3822e31d9f69416bc7c8c648929f1
wagnerelvio <wagnerelvio@gmail.com>
2026-03-11 13:04:19 -0300

    Create Code-01.js

---
+++ 11032026/Code-01.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b);  // 13
+console.log(a - b);  // 7
+console.log(a * b);  // 30
+console.log(a / b);  // 3.333...
+console.log(a % b);  // 1
+console.log(a ** b); // 1000
