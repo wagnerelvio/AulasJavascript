027a3281a70c8e5bd9e3f4badd58708ece2a9bcc
wagnerelvio <wagnerelvio@gmail.com>
2026-03-11 13:06:09 -0300

    Create Code-04.js

---
+++ 11032026/Code-04.js
@@ -0,0 +1,6 @@
+let idade = 20;
+let temCarteira = true;
+
+console.log(idade >= 18 && temCarteira); // true
+console.log(idade < 18 || temCarteira);  // true
+console.log(!temCarteira);               // false
