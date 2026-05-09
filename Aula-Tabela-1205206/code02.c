cd12fa7a2805bb97925efe67572a25332802b4e5
wagnerelvio <wagnerelvio@gmail.com>
2026-05-09 18:44:14 -0300

    Create code02.c

---
+++ Aula-Tabela-1205206/code02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define TAMANHO_TABELA 11
+
+// Funcao de Espalhamento Linear
+int funcaoEspalhamentoLinear(int chave) {
+    int a = 3;
+    int b = 7;
+    int m = TAMANHO_TABELA;
+
+    return (a * chave + b) % m;
+}
+
+int main() {
+    int chaves[] = {15, 25, 35, 45, 55, 68};
+    int quantidade = sizeof(chaves) / sizeof(chaves[0]);
+
+    printf("Funcao de Espalhamento Linear\n\n");
+
+    for (int i = 0; i < quantidade; i++) {
+        int indice = funcaoEspalhamentoLinear(chaves[i]);
+
+        printf("Chave: %d -> Indice na tabela: %d\n", chaves[i], indice);
+    }
+
+    return 0;
+}
