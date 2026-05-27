3bd5e49391bb9d65aa02cbf116e1f9c0efbfd096
wagnerelvio <wagnerelvio@gmail.com>
2026-05-27 19:49:03 -0300

    Create telegram.js

---
+++ Aula-27052026/telegram.js
@@ -0,0 +1,14 @@
+Leitura dos sensores:
+
+Sensor 1: {{$json.sensor1}} °C
+Sensor 2: {{$json.sensor2}} °C
+Sensor 3: {{$json.sensor3}} °C
+Sensor 4: {{$json.sensor4}} °C
+
+Média: {{$json.media}} °C
+Status: {{$json.status}}
+
+Falhas: {{$json.falhas}}
+Tempo sem atualização: {{$json.minutosSemAtualizacao}} minuto(s)
+
+Horário da última coleta: {{$json.horario}}
